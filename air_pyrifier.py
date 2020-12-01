d64da077804d27f0316a255d5ad857cf4ee79319
maeek <maciek299.ms@gmail.com>
2020-12-01 21:11:34 +0100

    Move files

--- air_pyrifier.py
+++ air_pyrifier.py
@@ -5,8 +5,8 @@ import os
 import pprint
 import sys
 import time
-from air_pyrifier_server import Air_pyrifier_server
-from air_pyrifier_client import Air_pyrifier_client
+from scripts.air_pyrifier_server import Air_pyrifier_server
+from scripts.air_pyrifier_client import Air_pyrifier_client
 
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -22,16 +22,16 @@ try:
 
         pp.pprint(results)
 
-    # # all of belows are working on air_set(list->dict) or ac.get(list)
     elif sys.argv[1] == 'status':
         results = Air_pyrifier_client().status()
 
         pp.pprint(results)
 
     elif sys.argv[1] == 'version':
-        result = VERSION
+        print(f'v{VERSION}')
 
-    else:  # sys.argv[1] in ['name','rhset','pwr','cl','mode','hud','ring','menu']:
+    else:
+        # sys.argv[1] in ['name','rhset','pwr','cl','mode','hud','ring','menu']:
         options = sys.argv[1:]
         results = Air_pyrifier_client().set(options)
 
